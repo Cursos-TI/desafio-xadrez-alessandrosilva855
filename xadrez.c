8dc76f8fb35f6a5d803f4e8327c4b0b1bbb35acb
alessandrosilva855 <alehalessandro09@gmail.com>
2025-04-11 15:40:42 -0300

    Update xadrez.c
    XadrezNivelMestreCompleto

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,64 @@
 #include <stdio.h>
+//função para mover a torre
+void moverTorre(int casas) {
+    if (casas > 0){
+        printf("Direita -> \n");
+        moverTorre(casas - 1);
+    }
+}
+//função para mover o bispo
+
+#include <stdio.h>
+
+// Função para mover Bispo que contém os DOIS FOR aninhados 
+void moverBispo(int vertical, int horizontal) {
+    for (int v = 0; v < vertical; v++) {      // For externo (Cima)
+        printf("Cima, ");
+        for (int h = 0; h < horizontal; h++) { // For interno (Direita)
+            printf("Direita!\n");
+        }
+    }
+}
+
+//função para mover a rainha
+void moverRainha(int casas){
+    if(casas > 0){
+        printf("<- Esquerda!\n");
+        moverRainha(casas - 1);
+    }
+}
+//função para mover o cavalo
+void moverCavalo(int movimentos){
+    printf("Cavalo: \n");
+
+    for (int cima = 0; cima < 2; cima++){
+        printf("Cima!\n");
+
+        if (cima == 1){ // Após o segundo movimento para cima
+            break; //sai do loopo de cima
+        }
+    }
+    printf("Direita!\n");
+    
+
+
+}
+
 
 int main(){
-    int bispo = 1;
-    int rainha = 1;
-    int cavalo = 1;
 
-    //Movendo o cavalo 'L'
-    printf("Cavalo: \n");
-    while (cavalo--)
-    {
-        for(int i = 0; i <2; i++){
-            printf("Cima \n"); //imprime Cima 2x
-        }
-        printf("Direita\n"); //imprime direita 1x
-    }
-    printf("---------------\n");
-
-    //mover a torre 5 casas para a direita
     printf("Torre: \n");
-    for (int i = 0; i < 5; i++){
-        printf("Direita! ->\n");
-    }
+    moverTorre(5);
     printf("---------------\n");
-
-    //mover o Bispo 5 casas na diagonal 
     printf("Bispo: \n");
-    while (bispo <= 5)
-    {
-        printf("Cima, Direita!\n");
-        bispo++;
-    }
+    moverBispo(5, 1);
     printf("---------------\n");
-
-    //Mover a rainha 8 Casas para a esquerda
     printf("Rainha: \n");
-    do
-    {
-        printf("<- Esquerda!\n");
-        rainha++;
-    } while (rainha <= 8);
+    moverRainha(8);
     printf("---------------\n");
-
+    moverCavalo(3);
+    printf("---------------\n");
     return 0;
-    
+
+
 }
